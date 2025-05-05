743723a34f62d6ed459339fb02dc7d9c589421e4
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-05 10:51:24 +0200

    add: api urls

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // API -> https://boolean-spec-frontend.vercel.app/freetestapi
+// città e paese -> /destinations?search=[query]
+// meteo -> /weathers?search={query}
+// aeroporto -> /airports?search={query}
 
 // start code
